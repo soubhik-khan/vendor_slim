arjun921/android_vendor_cm

Add pixel launcher and google wallpaper as preinstalled apps
Change-Id: I3dfa70cdb7afc07717fded5110748006b14f0e80

config/common.mk
@@ -41,6 +41,14 @@ endif
 PRODUCT_COPY_FILES += \
     vendor/cm/CHANGELOG.mkdn:system/etc/CHANGELOG-CM.txt
 
+# NexusLauncher
+PRODUCT_COPY_FILES += \
+    vendor/cm/prebuilt/NexusLauncher/NexusLauncher.apk:system/app/NexusLauncher/NexusLauncher.apk
+
+# Wallpaper
+PRODUCT_COPY_FILES += \
+    vendor/cm/prebuilt/Wallpaper/Wallpaper.apk:system/app/Wallpaper/Wallpaper.apk
+
 # Backup Tool
 PRODUCT_COPY_FILES += \
     vendor/cm/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \

soubhik-khan/vendor_slim

Change-Id: Idedb1c3fd404323d30db3b6bc0d0b3fc168ddb90
Conflicts:
	config/common.mk

config/common.mk
@@ -23,6 +23,14 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.config.ringtone=Titania.ogg \
     ro.config.notification_sound=Tethys.ogg
 
+# NexusLauncher
+PRODUCT_COPY_FILES += \
+    vendor/slim/prebuilt/NexusLauncher/NexusLauncher.apk:system/app/NexusLauncher/NexusLauncher.apk
+
+# Wallpaper
+PRODUCT_COPY_FILES += \
+    vendor/slim/prebuilt/Wallpaper/Wallpaper.apk:system/app/Wallpaper/Wallpaper.apk
+   
 # Backup Tool
 PRODUCT_COPY_FILES += \
     vendor/slim/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \